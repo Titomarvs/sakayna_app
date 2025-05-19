29d177e74590943b7569747e2be415fd4827e993
Marvic <mpajaganas2002@gmail.com>
2025-05-20 01:57:14 +0800

    comm

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-void main() => runApp(const MyApp());
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+void main() {
+  runApp(const SafeRideApp());
+}
+
+class SafeRideApp extends StatelessWidget {
+  const SafeRideApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: RiderTrackingMap(),
+    return MaterialApp(
+      title: 'Safe Ride',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SafeRideWebView(),
     );
   }
 }
 
-class RiderTrackingMap extends StatefulWidget {
-  const RiderTrackingMap({Key? key}) : super(key: key);
+class SafeRideWebView extends StatefulWidget {
+  const SafeRideWebView({super.key});
 
   @override
-  State<RiderTrackingMap> createState() => _RiderTrackingMapState();
+  State<SafeRideWebView> createState() => _SafeRideWebViewState();
 }
 
-class _RiderTrackingMapState extends State<RiderTrackingMap> {
-  final MapController _mapController = MapController();
-  LatLng? _currentPosition;
+class _SafeRideWebViewState extends State<SafeRideWebView> {
+  late final WebViewController _controller;
 
   @override
   void initState() {
     super.initState();
-    _determinePosition();
-  }
-
-  Future<void> _determinePosition() async {
-    LocationPermission permission = await Geolocator.checkPermission();
-
-    if (permission == LocationPermission.denied ||
-        permission == LocationPermission.deniedForever) {
-      permission = await Geolocator.requestPermission();
-      if (permission != LocationPermission.always &&
-          permission != LocationPermission.whileInUse) {
-        return;
-      }
-    }
-
-    Geolocator.getPositionStream(
-      locationSettings: const LocationSettings(
-        accuracy: LocationAccuracy.high,
-        distanceFilter: 5,
-      ),
-    ).listen((Position position) {
-      setState(() {
-        _currentPosition = LatLng(position.latitude, position.longitude);
-        _mapController.move(_currentPosition!, 17);
-      });
-    });
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://safe-ride-58f05.web.app/'));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('My Live Location')),
-      body: _currentPosition == null
-          ? const Center(child: CircularProgressIndicator())
-          : FlutterMap(
-              mapController: _mapController,
-              options: MapOptions(
-                initialCenter: _currentPosition!,
-                initialZoom: 17.0,
-              ),
-              children: [
-                TileLayer(
-                  urlTemplate:
-                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                  subdomains: const ['a', 'b', 'c'],
-                ),
-                MarkerLayer(
-                  markers: [
-                    Marker(
-                      point: _currentPosition!,
-                      width: 50,
-                      height: 50,
-                      child: const Icon(Icons.person_pin_circle,
-                          color: Colors.red, size: 50),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+      appBar: AppBar(title: const Text('Safe Ride')),
+      body: WebViewWidget(controller: _controller),
     );
   }
 }
